d34325d7486a157399d0848e8fbbcdbdafee2a31
DarkFate228 <86353980+DarkFate228@users.noreply.github.com>
2023-10-21 23:04:21 +0300

    Update ConsoleApplication1.cpp
    Nothing

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -1,20 +1,123 @@
-﻿// ConsoleApplication1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
 #include <iostream>
+    #include <iomanip>
 
-int main()
-{
-    std::cout << "Hello World!\n";
-}
+    using namespace std;
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+    
 
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
+    int GetFirstDay(int year);
+
+    void main()
+    {
+        int year, numdays;
+        int month = 1;
+        // getting the user to give the year
+        cout << "Enter year: ";
+        cin >> year;
+        cout << "\n\n";
+
+        // getting first day of given year
+        int First_Day = GetFirstDay(year);
+        while (month <= 12) {
+            switch (month) {
+            //Body
+            case 1:
+                numdays = 31;
+                cout << "January" << endl;
+                cout << "S  M  T  W  T  F  S" << endl;
+                cout << "-------------------" << endl << endl;
+                break;
+
+            case 2:
+                if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)) { numdays = 29; }
+                else { numdays = 28; }
+                cout << "Febrary" << endl;
+                cout << "S  M  T  W  T  F  S" << endl;
+                cout << "-------------------" << endl << endl;
+                break;
+
+            case 3:
+                numdays = 31;
+                cout << "March" << endl;
+                cout << "S  M  T  W  T  F  S" << endl;
+                cout << "-------------------" << endl << endl;
+                break;
+
+            case 4:
+                numdays = 30;
+                cout << "April"<< endl;
+                cout << "S  M  T  W  T  F  S" << endl;
+                cout << "-------------------" << endl << endl;
+                break;
+
+            case 5:
+                numdays = 31;
+                cout << "May" << endl;
+                cout << "S  M  T  W  T  F  S" << endl;
+                cout << "-------------------" << endl << endl;
+                break;
+
+            case 6:
+                numdays = 30;
+                cout << "June" << endl;
+                cout << "S  M  T  W  T  F  S" << endl;
+                cout << "-------------------" << endl << endl;
+                break;
+
+            case 7:
+                numdays = 31;
+                cout << "July" << endl;
+                cout << "S  M  T  W  T  F  S" << endl;
+                cout << "-------------------" << endl << endl;
+                break;
+
+            case 8:
+                numdays = 31;
+                cout << "August" << endl;
+                cout << "S  M  T  W  T  F  S" << endl;
+                cout << "-------------------" << endl << endl;
+                break;
+
+            case 9:
+                numdays = 30;
+                cout << "September" << endl;
+                cout << "S  M  T  W  T  F  S" << endl;
+                cout << "-------------------" << endl << endl;
+                break;
+
+            case 10:
+                numdays = 31;
+                cout << "October" << endl;
+                cout << "S  M  T  W  T  F  S" << endl;
+                cout << "-------------------" << endl << endl;
+                break;
+
+            case 11:
+                numdays = 31;
+                cout << "November" << endl;
+                cout << "S  M  T  W  T  F  S" << endl;
+                cout << "-------------------" << endl << endl;
+                break;
+
+            case 12:
+                numdays = 31;
+                cout << "December" << endl;
+                cout << "S  M  T  W  T  F  S" << endl;
+                cout << "-------------------" << endl << endl;
+                break;
+            }
+            month++;
+        }
+
+
+
+    }
+
+    int GetFirstDay(int year) {
+        int century = (year - 1) / 100;
+        int y = (year - 1) % 100;
+        int weekday = (((29 - (2 * century) + y + (y / 4) + (century / 4)) % 7) + 7) % 7;
+        // 0 would be sunday and 6 would be Saturday
+        return weekday;
+
+     }
